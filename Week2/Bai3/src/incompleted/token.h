a496667a58cc8c5c9774c6acede36c81b8df8045
Mingix-PT <mingixpt@gmail.com>
2024-11-27 09:36:04 +0700

    week 3

--- Week2/Bai3/src/incompleted/token.h
+++ Week2/Bai3/src/incompleted/token.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * @copyright (c) 2008, Hedspi, Hanoi University of Technology
  * @author Huu-Duc Nguyen
  * @version 1.0
@@ -14,7 +14,7 @@ typedef enum {
   TK_NONE, TK_IDENT, TK_NUMBER, TK_CHAR, TK_EOF,
 
   KW_PROGRAM, KW_CONST, KW_TYPE, KW_VAR,
-  KW_INTEGER, KW_CHAR, KW_ARRAY, KW_OF, 
+  KW_INTEGER, KW_CHAR, KW_ARRAY, KW_OF,
   KW_FUNCTION, KW_PROCEDURE,
   KW_BEGIN, KW_END, KW_CALL,
   KW_IF, KW_THEN, KW_ELSE,
@@ -24,7 +24,7 @@ typedef enum {
   SB_ASSIGN, SB_EQ, SB_NEQ, SB_LT, SB_LE, SB_GT, SB_GE,
   SB_PLUS, SB_MINUS, SB_TIMES, SB_SLASH,
   SB_LPAR, SB_RPAR, SB_LSEL, SB_RSEL
-} TokenType; 
+} TokenType;
 
 typedef struct {
   char string[MAX_IDENT_LEN + 1];
